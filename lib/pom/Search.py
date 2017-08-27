29854682af46b4553961f65d58ca4b2d5717225a
ihysk <sirouma.09@gmail.com>
2017-08-27 22:36:41 +0900

    Updated a function to get image URLs

--- lib/pom/Search.py
+++ lib/pom/Search.py
@@ -1,3 +1,5 @@
+import re
+import urllib.parse
 from lib.util.Logger import get_module_logger
 
 
@@ -11,6 +13,16 @@ class Search:
         return self.driver.find_element_by_id('search')
 
     def get_image_urls(self):
+        urls = []
         elems = self.get_search_results().find_elements_by_class_name('rg_l')
         for elem in elems:
-            self.logger.debug(elem.get_attribute('href'))
+            # Split href string with ? and &
+            parames = re.split('[?&]', elem.get_attribute('href'))
+            # URI decoded query parameter assigned to imgurl=
+            url = urllib.parse.unquote(parames[1].lstrip('imgurl='))
+            # Only param which has 'http(s)' will be stored in list
+            if url.find('http') != -1:
+                self.logger.debug(url)
+                urls.append(url)
+
+        return urls
