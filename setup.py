d4b8758ac4ab7a6f2682a07fb7bee65a4ef4e35c
ihysk <sirouma.09@gmail.com>
2017-09-05 23:57:02 +0900

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='image_scraper',
+    version='0.0.1',
+    packages='scraper',
+    entry_points={
+        'console_scripts': [
+            'imsc=scraper:scraping',
+        ],
+    },
+)
