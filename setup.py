a54a9763381cf9591764eea1352eeca6cf8a8ce7
ihysk <sirouma.09@gmail.com>
2017-09-08 23:47:38 +0900

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,14 @@ from setuptools import setup
 
 setup(
     name='image_scraper',
-    version='0.0.1',
+    version='0.0.2',
+    description='A sample Python project',
+    long_description='This is a sample to say Hello!',
+    url='https://github.com/ihysk/image_scraper',
+    author='ihysk',
+    author_email='sirouma.09@gmail.com',
+    license='MIT',
+    install_requires=['opencv-python', 'requests', 'selenium'],
     packages=find_packages(),
     entry_points={
         'console_scripts': [
