80ccccb7091d615083bebd392dd51e1a729b07db
ihysk <sirouma.09@gmail.com>
2017-09-18 23:21:27 +0900

    Use searched keyword as a filename

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -9,14 +9,16 @@ from lib.util.Logger import get_module_logger
 images_dir = 'images'
 
 
-def save_image(url):
+def save_image(url, name):
     response = requests.get(url)
 
     filename = re.split('/', url)
-    path = os.path.join(images_dir, filename[-1])
+    path, ext = os.path.splitext(filename[-1])
+    path = os.path.join(images_dir, name + ext)
 
     with open(path, "wb") as fout:
         fout.write(response.content)
+
 
 def scraping():
     driver = webdriver.Chrome(executable_path='/Users/ikuma/Downloads/chromedriver')
@@ -24,14 +26,16 @@ def scraping():
     logger = get_module_logger(__name__)
 
     home = Home(driver)
-    search = home.search_with_string(sys.argv[1])
+    keyword = sys.argv[1]
+    search = home.search_with_string(keyword)
     search.scroll_down_to_display_all_images()
     urls = search.get_image_urls()
 
-    for url in urls:
-        save_image(url)
+    for i, url in enumerate(urls):
+        save_image(url, keyword + '_' + str(i))
 
     driver.quit()
+
 
 if __name__ == '__main__':
     scraping()
